0d455fc79bc4a72e802c194d9bce23138cd8d03f
unknown <khawlaessalmi31@gmail.com>
2024-04-07 01:53:14 +0200

    Description de vos modifications2

--- src/main/java/myAppSpringBoot/Repositories/PersonnelAdministrationRepository.java
+++ src/main/java/myAppSpringBoot/Repositories/PersonnelAdministrationRepository.java
@@ -1,22 +1,11 @@
 package myAppSpringBoot.Repositories;
 
-<<<<<<< HEAD
-import org.springframework.stereotype.Repository;
-
 import myAppSpringBoot.Models.PersonnelAdministrationModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-=======
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import myAppSpringBoot.Models.PersonnelAdministrationModel;
->>>>>>> 6412ba8a87b9ae24ef76f6aaea2d09c002a391ac
-
 @Repository
 public interface PersonnelAdministrationRepository extends JpaRepository<PersonnelAdministrationModel, String> {
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 6412ba8a87b9ae24ef76f6aaea2d09c002a391ac
